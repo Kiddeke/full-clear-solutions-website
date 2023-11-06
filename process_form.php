a2f06a652668041207f3366689a993ece025ea84
grant <117955357+Kiddeke@users.noreply.github.com>
2023-11-05 19:27:45 -0500

    Add google analytics

--- process_form.php
+++ process_form.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:fcs-traffic.database.windows.net,1433; Database = fcs-traffic", "CloudSAe00b27c7", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:fcs-traffic.database.windows.net,1433; Database = fcs-traffic", "CloudSAe00b27c7", "{your_password_herSe}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
