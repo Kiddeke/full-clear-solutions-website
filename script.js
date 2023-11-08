b05adf23e36f50e6a081d2f66da8977e1cf3edf8
grant <117955357+Kiddeke@users.noreply.github.com>
2023-11-08 14:17:10 -0500

    Fix dark mode quote section

--- script.js
+++ script.js
@@ -10,33 +10,6 @@ setTimeout(() => {
   headerLogo.style.transform = 'translateX(0)';
 }, 500);
 
-// Animate the quote section on hover
-const quoteSection = document.querySelector('.quote');
-const shaqImage = document.querySelector('.shaqquote img');
-
-quoteSection.addEventListener('mouseover', () => {
-  quoteSection.style.backgroundColor = '#f9faf8';
-  quoteSection.style.color = '#125125';
-  shaqImage.style.animation = 'spin 2s infinite linear';
-});
-
-quoteSection.addEventListener('mouseout', () => {
-  quoteSection.style.backgroundColor = '#e5e7eb';
-  quoteSection.style.color = '#1f2937';
-  shaqImage.style.animation = 'none';
-});
-
-// Animate the signup button on click
-const signupButton = document.querySelector('.signupbottom');
-signupButton.addEventListener('click', () => {
-  signupButton.style.backgroundColor = '#125125';
-  signupButton.style.color = '#f9faf8';
-  setTimeout(() => {
-    signupButton.style.backgroundColor = '#0d4021';
-    signupButton.style.color = '#f9faf8';
-  }, 500);
-});
-
 function switchMode() {
   document.body.classList.toggle('dark');
 }
