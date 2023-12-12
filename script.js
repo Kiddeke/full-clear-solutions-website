2e3f4394ae75b98cd8bcd68e2ebfa97c4319a160
grant <117955357+Kiddeke@users.noreply.github.com>
2023-12-12 10:19:44 -0500

    Practice using arrays script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ setTimeout(() => {
   headerLogo.style.transform = 'translateX(0)';
 }, 500);
 
+
+//dark/light mode
 function switchModeDark() {
   if (document.body.classList == "") {
     document.body.classList.add("dark");
@@ -27,10 +29,41 @@ function switchModeLight() {
 }
 
 
+//store preferred mode
+
 if (localStorage.getItem("theme") == "dark") {
 document.body.classList.add("dark");
 } else {document.body.classList.add("light");
 };
 
+
+//array practice
+
+
+const cats = ["Oracle", " OneStream", " Anaplan", " Profix"];
+
+for (const cat of cats) {
+  console.log(cat);
+}
+
+function changeArray() {
+  if (cats[0] == "Hyperion") {
+    cats[0] = "Oracle";
+    console.log(cats[0]);
+    document.getElementById("demo").innerHTML = cats;
+  } else if (cats[0] == "Oracle") {
+    cats[0] = "Hyperion";
+    console.log(cats[0]);
+    document.getElementById("demo").innerHTML = cats;
+  }
+}
+
+document.getElementById("demo").innerHTML = cats
+
+cats[0] = "Hyperion";
+
+console.log(cats);
+
+console.log(cats[1]);
 
 
